e7b987f413a8c833c730eaaca348ca4c82ecb187
ayoub21dev <Ayoubcode941@gmail.com>
2025-09-16 09:15:28 +0100

    first commit

---
+++ Rappel.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+$input = [
+  'title'     => 'PHP 8 en pratique',
+  'excerpt'   => '',
+  'views'     => '300',
+  // 'published' absent
+  'author'    => 'Yassine'
+];
