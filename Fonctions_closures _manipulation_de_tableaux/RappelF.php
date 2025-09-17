47c056dce04fc44cc7a8ef3c4c78876af70c58d4
ayoub21dev <Ayoubcode941@gmail.com>
2025-09-17 09:12:33 +0100

    done

--- Fonctions_closures _manipulation_de_tableaux/RappelF.php
+++ Fonctions_closures _manipulation_de_tableaux/RappelF.php
@@ -7,3 +7,9 @@ $articles = [
   ['id'=>3,'title'=>'Composer & Autoload','category'=>'outils','views'=>90,'author'=>'Amina','published'=>false, 'tags'=>['composer','php']],
   ['id'=>4,'title'=>'Validation FormRequest','category'=>'laravel','views'=>210,'author'=>'Sara','published'=>true,  'tags'=>['laravel','validation']],
 ];
+
+function slugify(string $title): string {
+    $slug = strtolower($title);
+    $slug = preg_replace('/[^a-z0-9]+/i', '-', $slug);
+    return trim($slug, '-');
+}
